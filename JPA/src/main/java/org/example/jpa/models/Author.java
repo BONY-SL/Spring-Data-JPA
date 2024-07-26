32ec16e75ae081d3d3f784358d55346089338d84
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-07-26 23:59:35 +0530

    Update Author.java

--- JPA/src/main/java/org/example/jpa/models/Author.java
+++ JPA/src/main/java/org/example/jpa/models/Author.java
@@ -1,31 +1,54 @@
 package org.example.jpa.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-@Entity("Author")
-@Table("author")
+@Entity(name = "Author")
+@Table(name = "author")
 public class Author {
 
     @Id
+    @GeneratedValue/*(strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )*/
+
+    /*@SequenceGenerator(name = "author_sequence",
+    sequenceName = "author_sequence",
+    allocationSize = 1)*/
+
+    /*@TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )*/
     private Integer id;
 
 
+    @Column(name = "first_name")
     private String firstName;
 
 
+    @Column(name = "last_name")
     private String lastName;
 
 
+    @Column(unique = true,nullable = false)
     private String email;
 
 
     private int age;
+
+    @Column(updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
 
 }
